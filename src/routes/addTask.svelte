133a2a21b8e2a910185626317f3656162099239e
Chad Tennent <stealthbos@protonmail.com>
2023-03-27 14:38:48 -0700

    addTask component

---
+++ src/routes/addTask.svelte
@@ -0,0 +1,33 @@
+<script lang="typescript">
+	import moment from 'moment';
+
+	export let showAdd;
+	let id = 0;
+	let name = '';
+	let text = '';
+	let date;
+	let reminder = false;
+
+	// const taskStore = writable(tasks);
+</script>
+
+<div class="container">
+	<dialog open>
+		<article>
+			<form>
+				<div class="container">
+					<input type="text" bind:value={name} placeholder="task name" />
+					<input type="text" bind:value={text} placeholder="description" />
+					<input type="date" bind:value={date} placeholder="date" />
+					<input type="checkbox" on:click={() => (reminder = !reminder)} /> reminder
+				</div>
+				<button
+					on:click={() => {
+						console.log(showAdd);
+						showAdd = !showAdd;
+					}}>submit</button
+				>
+			</form>
+		</article>
+	</dialog>
+</div>
---
+++ src/routes/task.svelte
@@ -0,0 +1,23 @@
+<script lang="typescript">
+	let showMore = false;
+	export let task;
+</script>
+
+<article class="task">
+	<header>
+		<h4>{task.name}</h4>
+	</header>
+	<p>{task.date}</p>
+	{#if showMore}
+		<p>{task.text}</p>
+	{/if}
+	<footer>
+		<button
+			on:click={() => {
+				showMore = !showMore;
+				console.log(showMore);
+			}}
+			>show More
+		</button>
+	</footer>
+</article>
